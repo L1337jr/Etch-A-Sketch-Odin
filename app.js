1d777f25c99d063dc7b31ee720f1131f03be7328
L1337jr <luisdaniel1337jr@gmail.com>
2023-02-04 16:51:21 -0500

    modified:   app.js 	modified:   index.html 	modified:   styles.css

--- app.js
+++ app.js
@@ -1,47 +1,81 @@
 let newGrid = document.querySelector('.newGrid')
+let blackButton = document.querySelector('.black')
+let rainbowButton = document.querySelector('.rainbow')
 let container = document.querySelector('.grid-container')
-let gridItems = document.querySelectorAll('.grid-item')
+let range = document.querySelector('#range')
+let rangeValue = document.querySelector('.range-value')
 let gridSize = 16
+pen = false
+
+let currentMode = 'black'
+
+blackButton.onclick = () => {
+  changeMode('black')
+  blackButton.style.backgroundColor = "#636769"
+  blackButton.style.color = 'white'
+  rainbowButton.style.backgroundColor = 'white'
+  rainbowButton.style.color = 'black'
+}
 
 
-for (let i=0; i < gridSize ** 2; i++){
+rainbowButton.onclick = () => {
+  changeMode('rainbow')
+  rainbowButton.style.backgroundColor = "#636769"
+  rainbowButton.style.color = 'white'
+  blackButton.style.backgroundColor = 'white'
+  blackButton.style.color = 'black'
+}
+
+function changeMode(Mode){
+  currentMode = Mode
+
+}
+
+range.addEventListener("input", function() {
+  rangeValue.innerHTML = range.value + ' x ' + range.value;
+  container.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${range.value}, 1fr)`;
+  container.innerHTML = "";
+  gridSize = range.value
+  createGrid()
+});
+
+function createGrid(){
+  for (let i=0; i < gridSize ** 2; i++){
     let gridItem = document.createElement('div');
     gridItem.className = 'grid-item'
     container.appendChild(gridItem);
 };
+}
+
+
+container.addEventListener('click', function(){
+  if (pen == false){
+    pen = true
+  }
+  else pen = false
+})
+
 
 container.addEventListener("mouseover", (e) => {
-    if (e.target.classList.contains("grid-item")) {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-        }
- });
-
-gridItems.forEach(item => {
-    item.addEventListener("mouseover", function() {
-      let r = Math.floor(Math.random() * 256);
-      let g = Math.floor(Math.random() * 256);
-      let b = Math.floor(Math.random() * 256);
-      this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    });
- });
+  if (e.target.classList.contains("grid-item") && currentMode == 'rainbow' && pen == true) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+     e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+      }
+  else if (e.target.classList.contains("grid-item") && currentMode == 'black' && pen == true){
+    e.target.style.backgroundColor = 'black'
+  }
+});
 
 
 newGrid.addEventListener('click', function() {
-    let newGridSize = prompt("Enter the number of squares per side (max 100)");
-    if (newGridSize <= 100) {
-        container.innerHTML = "";
-        container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${newGridSize}, 1fr)`;
-        gridSize = newGridSize;
-        for (let i = 0; i < gridSize ** 2; i++) {
-          let gridItem = document.createElement("div");
-          gridItem.classList.add("grid-item");
-          container.appendChild(gridItem);
-        }
-      } else {
-        alert("Enter a number between 1 and 100");
-      }
+  container.innerHTML = "";
+  createGrid()
   });
+
+
+  window.onload = () => {
+    createGrid()
+  }
